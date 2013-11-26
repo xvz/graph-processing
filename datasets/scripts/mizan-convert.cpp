a3a8af23479e5bbc532147f4b972760c1278a6a7
Young Han <young.han@uwaterloo.ca>
2013-11-26 18:09:18 -0500

    Added some other "features" to conversion program.

--- datasets/scripts/mizan-convert.cpp
+++ datasets/scripts/mizan-convert.cpp
@@ -5,18 +5,50 @@
 #include <cerrno>
 
 
+#define M_IN_NOVAL       1
+#define M_IN_VAL         2
+#define M_IN_FAKE        3
+
 #define M_TO_GIRAPH      1
 #define M_TO_GPS_NOVAL   2
 #define M_TO_GPS_VAL     3
 
 using namespace std;
 
+static long counter = 1;
+
 static void usage(char **argv) {
-  cout << "usage: " << argv[0] << " input-file output-file format" << endl;
+  cout << "usage: " << argv[0] << " input-file output-file in-format out-format" << endl;
   cout << endl;
-  cout << "format: 1. Mizan to Giraph" << endl;
-  cout << "        2. Mizan to GPS (no values)" << endl;
-  cout << "        3. Mizan to GPS (vertex + edge values)" << endl;
+  cout << "in-format:  1. No values (node-id node-dst)" << endl;
+  cout << "            2. Values (node-id node-dst weight)" << endl;
+  cout << "            3. Same as 1, but generate unique sequential weights" << endl;
+  cout << "               (i.e., fake weights are assigned sequentially in" << endl;
+  cout << "                order of how vertices are listed in input file)" << endl;
+  cout << endl;
+  cout << "out-format: 1. Mizan to Giraph" << endl;
+  cout << "            2. Mizan to GPS (no values)" << endl;
+  cout << "            3. Mizan to GPS (vertex + edge values)" << endl;
+}
+
+static inline void get_edge_weight(ifstream &ifs, int in_format, long &edge_weight) {
+  switch (in_format) {
+  case M_IN_NOVAL:
+    edge_weight = 0;
+    break;
+
+  case M_IN_VAL:
+    ifs >> edge_weight;
+    break;
+
+  case M_IN_FAKE:
+    edge_weight = counter;
+    counter++;
+    break;
+
+  default:
+    cout << "Invalid in-format: " << in_format << "!" << endl;
+  }
 }
 
 /**
@@ -25,42 +57,51 @@ static void usage(char **argv) {
  * NOTE: Does not sort anything! (Sorting is not needed anyway)
  */
 int main(int argc, char **argv) {
-  if ( argc < 4 ) {
+  if ( argc < 5 ) {
     usage(argv);
     return -1;
   }
 
   ifstream ifs(argv[1], ifstream::in);
   ofstream ofs(argv[2], ofstream::out);
-  int format = atoi(argv[3]);
+  int in_format = atoi(argv[3]);
+  int out_format = atoi(argv[4]);
 
   if (!ifs || !ofs ||
-      (format < M_TO_GIRAPH || format > M_TO_GPS_VAL) ) {
+      (in_format < M_IN_NOVAL || in_format > M_IN_FAKE) ||
+      (out_format < M_TO_GIRAPH || out_format > M_TO_GPS_VAL) ) {
     usage(argv);
     return -1;
   }
   
-  int vertex_id, edge_dst;
-  int curr_id;
+  // longs, just to be safe
+  long vertex_id, edge_dst, edge_weight;
+  long curr_id;
+
+  // input format is either: vertex-id edge-dst
+  // or: vertex-id edge-dst edge-weight
 
   // first pair of reads
   ifs >> curr_id;
   ifs >> edge_dst;
-
-  switch (format) {
+  get_edge_weight(ifs, in_format, edge_weight);
+  
+  switch (out_format) {
   case M_TO_GIRAPH:
     while (!ifs.eof()) {
       // format: [vertex-id, vertex-val, [[edge-dst,edge-val],...]]
-      ofs << "[" << curr_id << ",0,[[" << edge_dst << ",0]";
-
+      ofs << "[" << curr_id << ",0,[[" << edge_dst << "," << edge_weight << "]";
+      
       ifs >> vertex_id;
       ifs >> edge_dst;
+      get_edge_weight(ifs, in_format, edge_weight);
 
       while ( vertex_id == curr_id && !ifs.eof() ) {
-        ofs << ",[" << edge_dst << ",0]";
+        ofs << ",[" << edge_dst << "," << edge_weight << "]";
 
         ifs >> vertex_id;
         ifs >> edge_dst;
+        get_edge_weight(ifs, in_format, edge_weight);
       }
 
       ofs << "]]" << endl;
@@ -77,12 +118,14 @@ int main(int argc, char **argv) {
 
       ifs >> vertex_id;
       ifs >> edge_dst;
+      get_edge_weight(ifs, in_format, edge_weight);
 
       while ( vertex_id == curr_id && !ifs.eof() ) {
         ofs << " " << edge_dst;
 
         ifs >> vertex_id;
         ifs >> edge_dst;
+        get_edge_weight(ifs, in_format, edge_weight);
       }
 
       ofs << endl;
@@ -95,16 +138,18 @@ int main(int argc, char **argv) {
   case M_TO_GPS_VAL:
     while (!ifs.eof()) {
       // format: vertex-id vertex-val edge-dst edge-val ...
-      ofs << curr_id << " 0 " << edge_dst << " 0";
+      ofs << curr_id << " 0 " << edge_dst << " " << edge_weight;
 
       ifs >> vertex_id;
       ifs >> edge_dst;
+      get_edge_weight(ifs, in_format, edge_weight);
 
       while ( vertex_id == curr_id && !ifs.eof() ) {
-        ofs << " " << edge_dst << " 0";
+        ofs << " " << edge_dst << " " << edge_weight;
 
         ifs >> vertex_id;
         ifs >> edge_dst;
+        get_edge_weight(ifs, in_format, edge_weight);
       }
 
       ofs << endl;
@@ -115,7 +160,7 @@ int main(int argc, char **argv) {
     break;
 
   default:
-    cout << "Invalid format: " << format << "!" << endl;
+    cout << "Invalid out-format: " << out_format << "!" << endl;
   }
 
   ifs.close();
