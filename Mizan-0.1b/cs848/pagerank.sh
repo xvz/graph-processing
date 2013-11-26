e94e1afebce4b4f8dc5f67ee083a4e58eee6a513
Young Han <young.han@uwaterloo.ca>
2013-11-26 21:35:42 +0000

    Push to avoid conflict.

--- Mizan-0.1b/cs848/pagerank.sh
+++ Mizan-0.1b/cs848/pagerank.sh
@@ -23,4 +23,4 @@ fi
 # should probably not go above 2, to avoid contention
 #
 # pagerank
-mpirun -f machines -np ${workers} ../Release/Mizan-0.1b -a 1 -s 300 -u ubuntu -g ${inputgraph} -w ${workers} -m ${dynamic} | tee -a ./${logfile}
+mpirun -f machines -np ${workers} ../Release/Mizan-0.1b -a 1 -s 100 -u ubuntu -g ${inputgraph} -w ${workers} -m ${dynamic} | tee -a ./${logfile}
--- gps-rev-110/cs848/sssp.sh
+++ gps-rev-110/cs848/sssp.sh
@@ -10,4 +10,4 @@ nodes=4
 cd ../master-scripts/
 
 # NOTE: max controls max number of supersteps
-./start_gps_nodes.sh ${nodes} quick-start "-ifs /user/ubuntu/gps-input/${inputgraph} -hcf /home/ubuntu/hadoop-1.0.4/conf/core-site.xml -jc gps.examples.sssp.SingleSourceAllVerticesShortestPathVertex###JobConfiguration -mcfg /user/ubuntu/gps-machine-config/cs848.cfg -log4jconfig /home/ubuntu/gps-rev-110/conf/log4j.config -other -max###300"
+./start_gps_nodes.sh ${nodes} quick-start "-ifs /user/ubuntu/gps-input/${inputgraph} -hcf /home/ubuntu/hadoop-1.0.4/conf/core-site.xml -jc gps.examples.sssp.SingleSourceAllVerticesShortestPathVertex###JobConfiguration -mcfg /user/ubuntu/gps-machine-config/cs848.cfg -log4jconfig /home/ubuntu/gps-rev-110/conf/log4j.config -other -max###100"
