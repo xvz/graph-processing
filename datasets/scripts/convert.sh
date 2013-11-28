4ebdde5ce9377743434d6e023b043fc9c58ea2f6
Young Han <young.han@uwaterloo.ca>
2013-11-27 19:11:00 -0500

    Oops, removed comments in script.

--- datasets/scripts/convert.sh
+++ datasets/scripts/convert.sh
@@ -11,19 +11,19 @@ SPECIALFILES=("retweet")
 BIGFILES=("livejournal" "orkut")
 
 
-#for file in "${FILES[@]}"; do
-#    echo "Converting ${file}"
-#    ../scripts/mizan-convert ${file}.txt ${file}-giraph.txt 3 1
-#    ../scripts/mizan-convert ${file}.txt ${file}-gps-noval.txt 3 2
-#    ../scripts/mizan-convert ${file}.txt ${file}-gps-val.txt 3 3
-#done
-# 
-#for file in "${SPECIALFILES[@]}"; do
-#    echo "Converting ${file}"
-#    ../scripts/mizan-convert ${file}.txt ${file}-giraph.txt 2 1
-#    ../scripts/mizan-convert ${file}.txt ${file}-gps-noval.txt 2 2
-#    ../scripts/mizan-convert ${file}.txt ${file}-gps-val.txt 2 3
-#done
+for file in "${FILES[@]}"; do
+    echo "Converting ${file}"
+    ../scripts/mizan-convert ${file}.txt ${file}-giraph.txt 3 1
+    ../scripts/mizan-convert ${file}.txt ${file}-gps-noval.txt 3 2
+    ../scripts/mizan-convert ${file}.txt ${file}-gps-val.txt 3 3
+done
+ 
+for file in "${SPECIALFILES[@]}"; do
+    echo "Converting ${file}"
+    ../scripts/mizan-convert ${file}.txt ${file}-giraph.txt 2 1
+    ../scripts/mizan-convert ${file}.txt ${file}-gps-noval.txt 2 2
+    ../scripts/mizan-convert ${file}.txt ${file}-gps-val.txt 2 3
+done
 
 read -p "Convert big files? (y/n): " yn
 
