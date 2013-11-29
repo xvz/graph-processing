96ebf9859bdf0c3bd993b1655b32496b2f26d489
Young Han <young.han@uwaterloo.ca>
2013-11-29 19:04:51 +0000

    Minor edits.

--- Mizan-0.1bu1/cs848/bench.sh
+++ Mizan-0.1bu1/cs848/bench.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 WORKERS=4
 
-./premizan.sh google.txt ${WORKERS} 1
-./premizan.sh amazon.txt ${WORKERS} 1
-./premizan.sh patents.txt ${WORKERS} 1
+#./premizan.sh google.txt ${WORKERS} 1
+#./premizan.sh amazon.txt ${WORKERS} 1
+#./premizan.sh patents.txt ${WORKERS} 1
 
 # use src 3858241
 #./sssp.sh patents.txt ${WORKERS} 1
@@ -17,13 +17,13 @@ WORKERS=4
 
 
 # use src 0
-./sssp.sh google.txt ${WORKERS} 1
-./sssp.sh google.txt ${WORKERS} 1
-./sssp.sh google.txt ${WORKERS} 1
-
-./sssp.sh google.txt ${WORKERS} 2
-./sssp.sh google.txt ${WORKERS} 2
-./sssp.sh google.txt ${WORKERS} 2
+#./sssp.sh google.txt ${WORKERS} 1
+#./sssp.sh google.txt ${WORKERS} 1
+#./sssp.sh google.txt ${WORKERS} 1
+# 
+#./sssp.sh google.txt ${WORKERS} 2
+#./sssp.sh google.txt ${WORKERS} 2
+#./sssp.sh google.txt ${WORKERS} 2
 
 # use src 0
 ./sssp.sh amazon.txt ${WORKERS} 1
@@ -36,28 +36,28 @@ WORKERS=4
 
 
 
-./pagerank.sh google.txt ${WORKERS} 1
-./pagerank.sh google.txt ${WORKERS} 1
-./pagerank.sh google.txt ${WORKERS} 1
-
-./pagerank.sh google.txt ${WORKERS} 2
-./pagerank.sh google.txt ${WORKERS} 2
-./pagerank.sh google.txt ${WORKERS} 2
-
-
-./pagerank.sh amazon.txt ${WORKERS} 1
-./pagerank.sh amazon.txt ${WORKERS} 1
-./pagerank.sh amazon.txt ${WORKERS} 1
-
-./pagerank.sh amazon.txt ${WORKERS} 2
-./pagerank.sh amazon.txt ${WORKERS} 2
-./pagerank.sh amazon.txt ${WORKERS} 2
-
-
-./pagerank.sh patents.txt ${WORKERS} 1
-./pagerank.sh patents.txt ${WORKERS} 1
-./pagerank.sh patents.txt ${WORKERS} 1
-
-./pagerank.sh patents.txt ${WORKERS} 2
-./pagerank.sh patents.txt ${WORKERS} 2
-./pagerank.sh patents.txt ${WORKERS} 2
+#./pagerank.sh google.txt ${WORKERS} 1
+#./pagerank.sh google.txt ${WORKERS} 1
+#./pagerank.sh google.txt ${WORKERS} 1
+# 
+#./pagerank.sh google.txt ${WORKERS} 2
+#./pagerank.sh google.txt ${WORKERS} 2
+#./pagerank.sh google.txt ${WORKERS} 2
+# 
+# 
+#./pagerank.sh amazon.txt ${WORKERS} 1
+#./pagerank.sh amazon.txt ${WORKERS} 1
+#./pagerank.sh amazon.txt ${WORKERS} 1
+# 
+#./pagerank.sh amazon.txt ${WORKERS} 2
+#./pagerank.sh amazon.txt ${WORKERS} 2
+#./pagerank.sh amazon.txt ${WORKERS} 2
+# 
+# 
+#./pagerank.sh patents.txt ${WORKERS} 1
+#./pagerank.sh patents.txt ${WORKERS} 1
+#./pagerank.sh patents.txt ${WORKERS} 1
+# 
+#./pagerank.sh patents.txt ${WORKERS} 2
+#./pagerank.sh patents.txt ${WORKERS} 2
+#./pagerank.sh patents.txt ${WORKERS} 2
--- Mizan-0.1bu1/cs848/recompile.sh
+++ Mizan-0.1bu1/cs848/recompile.sh
@@ -5,4 +5,8 @@ touch ../src/main.cpp
 cd ../Release
 make all
 
+for((i=2;i<=4;i++)); do
+  scp ../Release/Mizan-0.1b cloud${i}:~/Mizan-0.1bu1/Release/
+done
+
 echo "OK."
