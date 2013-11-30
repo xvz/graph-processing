fec163c1bf0f2ee56eac80d1e017f3e001dba9bd
Young Han <young.han@uwaterloo.ca>
2013-11-30 06:15:10 +0000

    Another Mizan bench.

--- Mizan-0.1bu1/cs848/bench.sh
+++ Mizan-0.1bu1/cs848/bench.sh
@@ -32,16 +32,28 @@ WORKERS=4
 #./wcc.sh patents.txt ${WORKERS} 2
 #./wcc.sh patents.txt ${WORKERS} 2
 
+./wcc.sh google.txt ${WORKERS} 3
+./wcc.sh google.txt ${WORKERS} 3
+./wcc.sh google.txt ${WORKERS} 3
+
+./wcc.sh amazon.txt ${WORKERS} 3
+./wcc.sh amazon.txt ${WORKERS} 3
+./wcc.sh amazon.txt ${WORKERS} 3
+
+./wcc.sh patents.txt ${WORKERS} 3
+./wcc.sh patents.txt ${WORKERS} 3
+./wcc.sh patents.txt ${WORKERS} 3
+
 
 
 # use src 3858241
-./sssp.sh patents.txt ${WORKERS} 1
-./sssp.sh patents.txt ${WORKERS} 1
-./sssp.sh patents.txt ${WORKERS} 1
+#./sssp.sh patents.txt ${WORKERS} 1
+#./sssp.sh patents.txt ${WORKERS} 1
+#./sssp.sh patents.txt ${WORKERS} 1
  
-./sssp.sh patents.txt ${WORKERS} 2
-./sssp.sh patents.txt ${WORKERS} 2
-./sssp.sh patents.txt ${WORKERS} 2
+#./sssp.sh patents.txt ${WORKERS} 2
+#./sssp.sh patents.txt ${WORKERS} 2
+#./sssp.sh patents.txt ${WORKERS} 2
 
 
 
@@ -64,6 +76,14 @@ WORKERS=4
 #./sssp.sh amazon.txt ${WORKERS} 2
 
 
+./sssp.sh amazon.txt ${WORKERS} 1
+./sssp.sh amazon.txt ${WORKERS} 1
+./sssp.sh amazon.txt ${WORKERS} 1
+
+#./sssp.sh google.txt ${WORKERS} 1
+#./sssp.sh google.txt ${WORKERS} 1
+#./sssp.sh google.txt ${WORKERS} 1
+
 
 #./pagerank.sh google.txt ${WORKERS} 1
 #./pagerank.sh google.txt ${WORKERS} 1
@@ -73,7 +93,6 @@ WORKERS=4
 #./pagerank.sh google.txt ${WORKERS} 2
 #./pagerank.sh google.txt ${WORKERS} 2
 # 
-# 
 #./pagerank.sh amazon.txt ${WORKERS} 1
 #./pagerank.sh amazon.txt ${WORKERS} 1
 #./pagerank.sh amazon.txt ${WORKERS} 1
@@ -90,3 +109,15 @@ WORKERS=4
 #./pagerank.sh patents.txt ${WORKERS} 2
 #./pagerank.sh patents.txt ${WORKERS} 2
 #./pagerank.sh patents.txt ${WORKERS} 2
+
+./pagerank.sh google.txt ${WORKERS} 3
+./pagerank.sh google.txt ${WORKERS} 3
+./pagerank.sh google.txt ${WORKERS} 3
+ 
+./pagerank.sh amazon.txt ${WORKERS} 3
+./pagerank.sh amazon.txt ${WORKERS} 3
+./pagerank.sh amazon.txt ${WORKERS} 3
+
+./pagerank.sh patents.txt ${WORKERS} 3
+./pagerank.sh patents.txt ${WORKERS} 3
+./pagerank.sh patents.txt ${WORKERS} 3
