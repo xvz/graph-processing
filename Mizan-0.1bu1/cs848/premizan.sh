2959ab8b09221ef9dfeb3474661179ad37b622f3
Young Han <young.han@uwaterloo.ca>
2014-01-16 20:18:41 +0000

    Another script update.

--- Mizan-0.1bu1/cs848/premizan.sh
+++ Mizan-0.1bu1/cs848/premizan.sh
@@ -18,13 +18,13 @@ cd ../preMizan/hadoopScripts/
 
 # modified from preMizan/preMizan.sh
 case $3 in
-    [1]*) ./hadoop_run_modhash.sh $inputgraph $2 true 2>&1 | tee -a ../../cs848/${logfile};;
-    [2]*) ./hadoop_run_range.sh $inputgraph $2 true 2>&1 | tee -a ../../cs848/${logfile};;
+    [1]*) ./hadoop_run_modhash.sh $inputgraph $2 true 2>&1 | tee -a ../../cs848/logs/${logfile};;
+    [2]*) ./hadoop_run_range.sh $inputgraph $2 true 2>&1 | tee -a ../../cs848/logs/${logfile};;
     *) echo "Error: invalid partition type!";;
 esac
 
 cd ../../cs848/
-touch premizan_${inputgraph}_${2}_${3}_"$(date +%F-%H-%M-%S)"_done.txt
+touch ./logs/premizan_${inputgraph}_${2}_${3}_"$(date +%F-%H-%M-%S)"_done.txt
 
 ## finish logging memory + network usage
 ./bench_finish.sh ${logname}
