99836eec6e48cb31e175b3c3b1a0f6ff010ccab7
Young Han <young.han@uwaterloo.ca>
2014-01-16 17:05:40 +0000

    Updated hadoop scripts.

--- hadoop-1.0.4/cs848/update-hosts.sh
+++ hadoop-1.0.4/cs848/update-hosts.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-nodes=12
+hostname=$(hostname)
+
+if [[ "$hostname" == "cloud0" ]]; then
+    name=cloud
+    nodes=4
+elif [[ "$hostname" == "cld0" ]]; then
+    name=cld
+    nodes=8
+elif [[ "$hostname" == "c0" ]]; then
+    name=c
+    nodes=16
+else
+    echo "Invalid hostname"
+    exit -1
+fi
 
 for ((i=1;i<=${nodes};i++)); do
-    sudo scp /etc/hosts cloud$i:/etc/hosts
-    sudo ssh cloud$i "echo \"cloud${i}\" > /etc/hostname"
+    sudo scp /etc/hosts ${name}$i:/etc/hosts
+    sudo ssh ${name}$i "echo \"${name}${i}\" > /etc/hostname"
 done
--- hadoop-1.0.4/cs848/update-xml.sh
+++ hadoop-1.0.4/cs848/update-xml.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -e
 
-nodes=4
+hostname=$(hostname)
+
+if [[ "$hostname" == "cloud0" ]]; then
+    name=cloud
+    nodes=4
+elif [[ "$hostname" == "cld0" ]]; then
+    name=cld
+    nodes=8
+elif [[ "$hostname" == "c0" ]]; then
+    name=c
+    nodes=16
+else
+    echo "Invalid hostname"
+    exit -1
+fi
 
 cd ~/hadoop-1.0.4/conf/
 
 for ((i=1;i<=${nodes};i++)); do
-    scp ./* cloud$i:~/hadoop-1.0.4/conf/
+    scp ./* ${name}$i:~/hadoop-1.0.4/conf/
 done
