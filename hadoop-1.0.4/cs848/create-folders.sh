a68767ecb77f7180998cf0bb0f7089dbc817f17d
Young Han <young.han@uwaterloo.ca>
2014-01-16 20:06:05 +0000

    New script.

---
+++ hadoop-1.0.4/cs848/create-folders.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+hostname=$(hostname)
+
+if [[ "$hostname" == "cloud0" ]]; then
+    name=cloud
+    nodes=4
+elif [[ "$hostname" == "cld0" ]]; then
+    name=cld
+    nodes=8
+elif [[ "$hostname" == "c0" ]]; then
+    name=c
+    nodes=16
+else
+    echo "Invalid hostname"
+    exit -1
+fi
+
+for ((i=1;i<=${nodes};i++)); do
+    ssh ${name}$i "mkdir -p ~/giraph-1.0.0/cs848/logs"
+    ssh ${name}$i "mkdir -p ~/gps-rev-110/cs848/logs"
+    ssh ${name}$i "mkdir -p ~/Mizan-0.1bu1/cs848/logs"
+done
