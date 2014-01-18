567c86e36dce97363ffcc3402d9d85177f83deb6
Young Han <young.han@uwaterloo.ca>
2014-01-17 22:03:19 -0500

    Another update to GPS dimest script.

--- gps-rev-110/cs848/dimest.sh
+++ gps-rev-110/cs848/dimest.sh
@@ -22,7 +22,7 @@ logfile=${logname}.txt       # GPS statistics (incl running time)
 cd ../master-scripts/
 
 # NOTE: max controls max number of supersteps
-# max must be 30, to match Giraph
+# max must be 100, to match Giraph
 ./start_gps_nodes.sh ${nodes} quick-start "-ifs /user/ubuntu/gps-input/${inputgraph} -hcf /home/ubuntu/hadoop-1.0.4/conf/core-site.xml -jc gps.examples.dimest.DiameterEstimationVertex###JobConfiguration -mcfg /user/ubuntu/gps-machine-config/cs848.cfg -log4jconfig /home/ubuntu/gps-rev-110/conf/log4j.config -other -max###100"
 
 ## finish logging memory + network usage
