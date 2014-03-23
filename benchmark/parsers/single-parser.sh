8d1f8c786edd0932fc313d12034eeb51d12a467c
Young Han <young.han@uwaterloo.ca>
2014-03-23 13:00:07 -0400

    Changed time parsing to use more robust awk. Fixed memory parser bug (using wrong min/init values) and IO/running time print out being swapped.

--- benchmark/parsers/single-parser.sh
+++ benchmark/parsers/single-parser.sh
@@ -80,10 +80,10 @@ done
 # Time parsing, unique for each system
 #######################################
 if [[ $system -eq $SYS_GIRAPH ]]; then
-    setup=$(grep "Setup " "${logname}_time.txt" | cut -c 67-100)
-    input=$(grep "Input superstep " "${logname}_time.txt" | cut -c 77-100)
-    shutdown=$(grep "Shutdown " "${logname}_time.txt" | cut -c 70-100)
-    total=$(grep "Total (mil" "${logname}_time.txt" | cut -c 67-100)
+    setup=$(grep "Setup " "${logname}_time.txt" | sed "s/.*(milliseconds)=//g")
+    input=$(grep "Input superstep " "${logname}_time.txt" | sed "s/.*(milliseconds)=//g")
+    shutdown=$(grep "Shutdown " "${logname}_time.txt" | sed "s/.*(milliseconds)=//g")
+    total=$(grep "Total (mil" "${logname}_time.txt" | sed "s/.*(milliseconds)=//g")
 
     # all times are in ms, so have to convert it to seconds
     # must use perl (or something else) as $((..)) can't handle floats
@@ -92,9 +92,9 @@ if [[ $system -eq $SYS_GIRAPH ]]; then
     time_run=$(perl -e "print( $time_tot - $time_io )")
 
 elif [[ $system -eq $SYS_GPS ]]; then
-    start=$(grep "^SYSTEM_START_TIME " "${logname}_time.txt" | cut -c 19-100 | sed "s/ true//g")
-    computestart=$(grep "^START_TIME " "${logname}_time.txt" | cut -c 12-100 | sed "s/ true//g")
-    end=$(grep -- "^-1-LATEST_STATUS_TIMESTAMP " "${logname}_time.txt" | cut -c 28-100 | sed "s/ true//g")
+    start=$(grep "^SYSTEM_START_TIME " "${logname}_time.txt" | awk '{print $2}')
+    computestart=$(grep "^START_TIME " "${logname}_time.txt" | awk '{print $2}')
+    end=$(grep -- "^-1-LATEST_STATUS_TIMESTAMP " "${logname}_time.txt" | awk '{print $2}')
 
     # all times are in ms, so have to convert it to seconds
     # must use perl (or something else) as $((..)) can't handle floats
@@ -103,20 +103,20 @@ elif [[ $system -eq $SYS_GPS ]]; then
     time_run=$(perl -e "print( $time_tot - $time_io )")
 
 elif [[ $system -eq $SYS_GRAPHLAB ]]; then
-    time_tot=$(grep "TOTAL TIME (sec)" "${logname}_time.txt" | sed "s/.*: //g")
-    time_run=$(grep "Finished Running engine" "${logname}_time.txt" | sed "s/.* in //g" | sed "s/ seconds.//g")
+    time_tot=$(grep "TOTAL TIME (sec)" "${logname}_time.txt" | awk '{print $4}')
+    time_run=$(grep "Finished Running engine" "${logname}_time.txt" | awk '{print $5}')
     time_io=$(perl -e "print( $time_tot - $time_run )")
 
 elif [[ $system -eq $SYS_MIZAN ]]; then
     # Mizan's premizan logs are distinct from normal algorithm runs
     alg=$(echo "$logname" | sed "s/_.*//g")
     if [[ "$alg" == "premizan" ]]; then
-        time_tot=$(grep "TOTAL TIME (sec)" "${logname}_time.txt" | sed "s/.*: //g")
-        time_io=0
-        time_run=$time_tot
+        time_tot=$(grep "TOTAL TIME (sec)" "${logname}_time.txt" | awk '{print $4}')
+        time_io=$time_tot
+        time_run=0
     else
-        time_run=$(grep "TIME: Total Running Time without IO = " "${logname}_time.txt" | cut -c 44-100)
-        time_tot=$(grep "TIME: Total Running Time =" "${logname}_time.txt" | cut -c 33-100)
+        time_run=$(grep "TIME: Total Running Time without IO = " "${logname}_time.txt" | awk '{print $8}')
+        time_tot=$(grep "TIME: Total Running Time =" "${logname}_time.txt" | awk '{print $6}')
         time_io=$(($time_tot - $time_run))
     fi
 fi
@@ -134,8 +134,8 @@ FILES=(${logname}_{[0-9][0-9],[1-9]}_mem.txt)
 mem=""
 for file in "${FILES[@]}"; do
     maxmem=$(awk '{print $3}' "$file" | sort -rn | head -n 1)
-    minmem=$(awk '{print $3}' "$file" | sort -n | head -n 4 | tail -n 1)
-    initmem=$(awk '{print $3}' "$file" | head -n 4 | tail -n 1)
+    minmem=$(awk '{print $3}' "$file" | sort -n | head -n 1)
+    initmem=$(awk '{print $3}' "$file" | head -n 1)
 
     # convert largest difference into GBytes, and add it on to the existing results
     mem="${mem} $(perl -e "print( (${maxmem} - ${minmem})/(1024*1024) )")"
@@ -170,6 +170,7 @@ for file in "${FILES[@]}"; do
     losentsum=$((${losentsum} + ${lo[26]} - ${lo[9]}))
 done
 
+# ethernet is the relevant network I/O measure (in GBytes)
 eth_recv=$(perl -e "print( ${ethrecvsum}/(1024*1024*1024) )")
 eth_sent=$(perl -e "print( ${ethsentsum}/(1024*1024*1024) )")
 
@@ -178,10 +179,10 @@ eth_sent=$(perl -e "print( ${ethsentsum}/(1024*1024*1024) )")
 # Output results
 ##################
 echo "------------------------------------------------------------------------------------------------"
-echo " Total time |   IO time |  Running time |   Mem per worker (min/max/avg) |  Net I/O (recv/sent) "
+echo " Total time |   IO time |  Running time |  Mem per worker (min/max/avg)  |  Net I/O (recv/sent) "
 echo "------------+-----------+---------------+--------------------------------+----------------------"
 printf "  %8.2fs | %8.2fs |     %8.2fs | %7.3f / %7.3f / %7.3f GB | %7.3f / %7.3f GB \n" \
-    $time_tot $time_run $time_io \
+    $time_tot $time_io $time_run \
     $mem_min $mem_max $mem_avg \
     $eth_recv $eth_sent
 echo "------------------------------------------------------------------------------------------------"
