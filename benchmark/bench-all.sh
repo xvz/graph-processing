efd8300493761572373e8b7274b38b537a7d8a8a
Young Han <young.han@uwaterloo.ca>
2014-04-02 15:08:51 +0000

    Fixed disown and fg comment.

--- benchmark/bench-all.sh
+++ benchmark/bench-all.sh
@@ -6,11 +6,15 @@
 # The batch-benchmarking scripts are quite primitive, simply because
 # when things fail it's usually easier to intervene manually.
 #
+#
 # We recommend running this with disown or nohup, so that a broken
 # ssh connection doesn't kill the job. For example:
 # ./bench-all.sh &      (this should output "[X] ####"---X is usually 1)
 # disown -h %X          (where X is from above)
-# fg                    (to place job back into foreground)
+#
+# Note: disown -h allows the task to be brought back to fg in the
+# same shell session, but be aware that this *will* kill the task if
+# the ssh connection cuts out!
 
 read -p "Press enter to continue..."
 
