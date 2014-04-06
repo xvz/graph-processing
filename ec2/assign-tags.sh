26f7ec18c4f2ccbd1a0a6c2a7cd32ea3add1baa5
Young Han <young.han@uwaterloo.ca>
2014-04-06 14:05:33 -0400

    Also tag master's EBS volume. Everything is now spot instances.

--- ec2/assign-tags.sh
+++ ec2/assign-tags.sh
@@ -2,6 +2,7 @@
 
 # This assigns the proper tags to spot instances after they are launched,
 # by using the tags applied to the associated spot instance requests.
+# Additionally, this tags the master's EBS volume.
 #
 # By default, only spot instance *requests* are tagged---the resulting
 # launched instances remain untagged.
@@ -27,10 +28,16 @@ case ${NUM_MACHINES} in
     *) echo "Invalid option!"; exit -1;;
 esac
 
-MASTER_ID=($(aws ec2 describe-spot-instance-requests --filter "Name=tag:Name,Values=${name}0" \
-               | grep "InstanceId" | awk '{print $2}' | sed -e 's/",*//g' | tr '\n' ' '))
+MASTER_ID=$(aws ec2 describe-spot-instance-requests --filter "Name=tag:Name,Values=${name}0" \
+             | grep "InstanceId" | awk '{print $2}' | sed -e 's/",*//g')
 WORKER_IDS=($(aws ec2 describe-spot-instance-requests --filter "Name=tag:Name,Values=${name}" \
                | grep "InstanceId" | awk '{print $2}' | sed -e 's/",*//g' | tr '\n' ' '))
 
-aws ec2 create-tags --resources "${MASTER_ID}" --tags Key=Name,Value=${name}0
-aws ec2 create-tags --resources "${WORKER_IDS[@]}" --tags Key=Name,Value=${name}
+aws ec2 create-tags --resources "$MASTER_ID" --tags Key=Name,Value=${name}0 &
+aws ec2 create-tags --resources "${WORKER_IDS[@]}" --tags Key=Name,Value=${name} &
+
+MASTER_VOL=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=${name}0" \
+              | grep "VolumeId" | awk '{print $2}' | sed -e 's/",*//g')
+
+aws ec2 create-tags --resources "$MASTER_VOL" --tags Key=Name,Value=${name}0 &
+wait
