298126eecd792cbbe75a31543fe7bdf773833a9f
Young Han <young.han@uwaterloo.ca>
2014-04-07 04:02:51 +0000

    Removed slaves file depedencies due to a strange bash "bug". Changed GPS ports to 64xxx to better avoid conflicts.

--- benchmark/gps/init.sh
+++ benchmark/gps/init.sh
@@ -2,33 +2,25 @@
 
 # Initiate GPS by creating slaves and machine config files.
 #
-# NOTE: "slaves" is NOT placed in master-script/, because we use
-# our own scripts for starting/stopping GPS workers.
+# NOTE: "slaves" is no longer needed, because we use our own 
+# scripts for starting/stopping GPS workers.
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source ../common/get-hosts.sh
 source ../common/get-dirs.sh
 source ../common/get-configs.sh
 
-rm -f slaves
 rm -f machine.cfg
 
-# create slaves file
-for ((i = 1; i <= ${machines}; i++)); do
-    for ((j = 1; j <= ${GPS_WPM}; j++)); do
-        echo "${name}${i}" >> slaves
-    done
-done
-
 # create machine config file
-echo "-1 ${name}0 55000" >> machine.cfg   # master is special
+echo "-1 ${name}0 64000" >> machine.cfg   # master is special
 
 w_id=0    # worker counter (needed if workers per machine > 1)
 for ((i = 1; i <= ${machines}; i++)); do
     # to get multiple workers per machine, use the same name
     # but give it a unique id and port
     for ((j = 1; j <= ${GPS_WPM}; j++)); do
-        echo "${w_id} ${name}${i} $((55001 + ${w_id}))" >> machine.cfg
+        echo "${w_id} ${name}${i} $((64001 + ${w_id}))" >> machine.cfg
         w_id=$((w_id+1))
     done
 done
--- benchmark/gps/stop-nodes.sh
+++ benchmark/gps/stop-nodes.sh
@@ -2,11 +2,15 @@
 
 # Does the same thing as master-scripts/stop_gps_nodes.sh, but faster.
 # Also removes the need for a separate scripts/stop_nodes.sh.
+#
+# NOTE: we do not use a slave file, as we only need to ssh to each
+# machine once to kill all its workers.
+
+source $(dirname "${BASH_SOURCE[0]}")/../common/get-hosts.sh
 
 kill -9 $(ps aux | grep "[g]ps_node_runner" | awk '{print $2}')
 
-# the "|| ..." is a workaround in case the file doesn't end with a newline
-while read slave || [ -n "$slave" ]; do
-    ssh $slave "kill -9 \$(ps aux | grep \"[g]ps_node_runner\" | awk '{print \$2}')" &
-done < "$(dirname "${BASH_SOURCE[0]}")"/slaves
+for ((i = 1; i <= ${machines}; i++)); do
+    ssh ${name}${i} "kill -9 \$(ps aux | grep \"[g]ps_node_runner\" | awk '{print \$2}')" &
+done
 wait
