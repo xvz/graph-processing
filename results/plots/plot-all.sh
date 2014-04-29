407fedae3760e872306b6573da6e2c59e9f84f9c
Young Han <young.han@uwaterloo.ca>
2014-04-28 23:13:25 -0400

    Updated plot script with better font sizes; added flattened legend plot. For all-plots, added legend on every page, shifted AR to be on the bottom row.

--- results/plots/plot-all.sh
+++ results/plots/plot-all.sh
@@ -7,6 +7,9 @@
 ./plot.py 2 --save-eps --plot-sum
 ./plot.py 2 --save-eps
 
+./plot-with-cuts.py 0 --save-eps
+./plot-with-cuts.py 2 --save-eps
+
 ./plot.py 1 --master --save-eps
 ./plot.py 2 --master --save-eps
 
