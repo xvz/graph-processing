fa2022dc351c12458910f0649acdfad55bd60a02
Young Han <young.han@uwaterloo.ca>
2014-05-04 12:33:21 -0400

    Refactor: missed a file.

--- benchmark/common/ssh-check.sh
+++ benchmark/common/ssh-check.sh
@@ -6,6 +6,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 source ./get-hosts.sh
 source ./get-dirs.sh
 
-for ((i = 1; i <= ${machines}; i++)); do
-    nc -v -w 1 ${name}${i} -z 22
+for ((i = 1; i <= ${NUM_MACHINES}; i++)); do
+    nc -v -w 1 ${CLUSTER_NAME}${i} -z 22
 done
